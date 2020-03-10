aacd0f1b7996511f3613d195b123d97a87c78cae
Charlie Wu <c.wu@webfm.net>
2020-03-10 16:59:16 +1100

    prevent file with same name been imported and export correct core ml json

--- ImagePreparation/ViewController/ViewController+Actions.swift
+++ ImagePreparation/ViewController/ViewController+Actions.swift
@@ -49,7 +49,34 @@ extension ViewController {
         document?.save(self)
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-        try? encoder.encode(impSet.annotations).write(to: FileHelper.annotationsUrl(workFolder: impSet.workFolder))
+        var dataSet = [Any]()
+        
+        // start new code to export data which works with CreateML
+        impSet.annotations.annotations.enumerated().forEach{ (index, element) in
+            var obj = [String: Any]()
+            var annotations = [Any]()
+            var annotationJson = [String:Any]()
+            var coordinates = [String:Any]()
+            
+            coordinates["x"] = element.coordinates.x
+            coordinates["y"] = element.coordinates.x
+            coordinates["width"] = element.coordinates.width
+            coordinates["height"] = element.coordinates.height
+            
+            annotationJson["label"] = element.label
+            annotationJson["coordinates"] = coordinates
+            annotations.append(annotationJson)
+            obj["image"] = impSet.annotations.path[index]
+            obj["annotations"] = annotations
+            dataSet.append(obj)
+        }
+        let json = try! JSONSerialization.data(withJSONObject: dataSet, options: .prettyPrinted)
+        try! json.write(to: FileHelper.annotationsUrl(workFolder: impSet.workFolder))
+        // end new code to export data which works with CreateML
+        
+//        try? encoder.encode(impSet.annotations).write(to: FileHelper.annotationsUrl(workFolder: impSet.workFolder))
+//        try? encoder.encode(dataSet).write(to: FileHelper.annotationsUrl(workFolder: impSet.workFolder))
+        
         var exportUrl = url.appendingPathComponent(AppConfiguration.mlExportDirName)
         var count = 2
         while FileManager.default.fileExists(atPath: exportUrl.path) {
@@ -88,32 +115,38 @@ extension ViewController {
         var annotationList:[Annotation] = []
         var mlPaths:[String] = []
         
-        urls.forEach { (fileUrl) in
-            guard let targetImageUrl = document?.impSet.imageFolder.appendingPathComponent(fileUrl.lastPathComponent),
-                let image = NSImage(contentsOf: fileUrl),
-                image.size.width > 0, image.size.height > 0,
-                Double(image.size.width) < Double.infinity,
-                Double(image.size.height) < Double.infinity else {
-                    return
+        urls
+            .filter { fileUrl in
+                let path = fileUrl.pathComponents.last!
+                let pathList = document?.impSet.annotations.path.map { $0.components(separatedBy: "/").last! }
+                return !(pathList?.contains(path) ?? false)
             }
-            let width = image.size.width
-            let height = image.size.height
-            let scale = max(AppConfiguration.normalizedBaseSizeInPixels / width,
-                            AppConfiguration.normalizedBaseSizeInPixels / height)
-            let targetSize = NSSize(width: width * scale, height: height * scale)
-            let resized = image.resizedImage(w: targetSize.width, h: targetSize.height)
-            resized.writeToFile(file: targetImageUrl, usingType: .jpeg)
-            let mlPath:String = targetImageUrl.pathComponents.suffix(2).joined(separator: "/")
-            mlPaths.append(mlPath)
-            let pixelSize = resized.pixelSize()
-            let coordinates = Coordinates(width: Double(pixelSize.width), height: Double(pixelSize.height),
-                                          x: Double(pixelSize.width) / 2.0, y: Double(pixelSize.height) / 2.0)
-            let annotation = Annotation(coordinates: coordinates, label: AppConfiguration.defaultLabel)
-            annotationList.append(annotation)
+            .forEach { (fileUrl) in
+                guard let targetImageUrl = document?.impSet.imageFolder.appendingPathComponent(fileUrl.lastPathComponent),
+                    let image = NSImage(contentsOf: fileUrl),
+                    image.size.width > 0, image.size.height > 0,
+                    Double(image.size.width) < Double.infinity,
+                    Double(image.size.height) < Double.infinity else {
+                        return
+                }
+                let width = image.size.width
+                let height = image.size.height
+                let scale = max(AppConfiguration.normalizedBaseSizeInPixels / width,
+                                AppConfiguration.normalizedBaseSizeInPixels / height)
+                let targetSize = NSSize(width: width * scale, height: height * scale)
+                let resized = image.resizedImage(w: targetSize.width, h: targetSize.height)
+                resized.writeToFile(file: targetImageUrl, usingType: .jpeg)
+                let mlPath:String = targetImageUrl.pathComponents.suffix(2).joined(separator: "/")
+                mlPaths.append(mlPath)
+                let pixelSize = resized.pixelSize()
+                let coordinates = Coordinates(width: Double(pixelSize.width), height: Double(pixelSize.height),
+                                              x: Double(pixelSize.width) / 2.0, y: Double(pixelSize.height) / 2.0)
+                let annotation = Annotation(coordinates: coordinates, label: AppConfiguration.defaultLabel)
+                annotationList.append(annotation)
+            }
+            document?.impSet.annotations.annotations.append(contentsOf: annotationList)
+            document?.impSet.annotations.path.append(contentsOf: mlPaths)
         }
-        document?.impSet.annotations.annotations.append(contentsOf: annotationList)
-        document?.impSet.annotations.path.append(contentsOf: mlPaths)
-    }
     
     private func folderOpenPanel() -> NSOpenPanel {
         let openPanel = NSOpenPanel()
